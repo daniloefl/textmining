e5b76c7256cd46e142ff66293cb9d9cc8765f66f
Danilo Ferreira de Lima <daniloefl@gmail.com>
2017-03-21 13:03:03 +0100

    Added similarity comparison.

--- mine.py
+++ mine.py
@@ -4,15 +4,25 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
 from nltk.stem.porter import PorterStemmer
-from gensim import corpora, models
+from gensim import corpora, models, similarities
 import os
 import utils
 
-ntopics = 20
+ntopics = 10       # number of topics to split the imput documents on
+useLDA  = False    # whether to use Latent Dirichlet Allocation or LSI
 
 def main():
+
+  # first of all transform HTML into plain text
+  # to remove all the HTML tags
+  # then use the clean() function of the Source class
+  # to clean up rubbish (ie: navigation bars, advertisement)
+  # the document is stored in data/[date]/[source tag]/frond_clean.txt
+  # the document's clean text is in doc_set and its date and source are kept with
+  # the same index in doc_id
   doc_set = []
   doc_id = []
+  date_set = {}
   dirlist = os.listdir('data/')
   dirlist.sort()
   for day in dirlist:
@@ -26,7 +36,14 @@ def main():
       for i in f.readlines(): doc += i
       doc_set.append(doc)
       doc_id.append('%s/%s' % (day, s))
-  
+      if not day in date_set: date_set[day] = []
+      date_set[day].append(len(doc_id)-1)
+
+  # now we need to split it into words
+  # and remove the word ending, so that only the stem of the word remains
+  # this removes differences between masculin and feminin and plural/singular
+  # we also remove common articles, prepositions, etc, which happen too often and carry no
+  # meaning in the "bad-of-words" approach
   tokenizer = RegexpTokenizer(r'\w+')
   
   # Create p_stemmer of class PorterStemmer
@@ -34,49 +51,117 @@ def main():
   
   texts = []
   for doc in doc_set:
-    raw = doc.lower()
-    tokens = tokenizer.tokenize(raw)
-    lang_stop = set(stopwords.words('portuguese'))
-    stopped_tokens = [i for i in tokens if not i in lang_stop]
+    raw = doc.lower() # to lower case
+    tokens = tokenizer.tokenize(raw) # make word tokens and save it in a list
+    lang_stop = set(stopwords.words('portuguese')) # get set of stop words for portuguese
+    stopped_tokens = [i for i in tokens if not i in lang_stop] # remove stop words
   
     # stem token
     text = [p_stemmer.stem(i) for i in stopped_tokens]
-    texts.append(text)
+    texts.append(text) # texts keeps a list of list of words (same indexing as doc_set and doc_id)
   
-  #print texts
+  # now make a dictionary of words found
+  # this assigns a unique integer to each word
   dictionary = corpora.Dictionary(texts)
+  # we can use dictionary.token2id to get the list of word-id mapping
+  # doc2bow counts how many times a word appears in the text and makes a list of counts of words
+  # this is now closer to a vector interpretation of each document
   corpus = [dictionary.doc2bow(text) for text in texts]
+  # we would now like to use the tf-idf transformation for each document representation
+  # this weights more words that appear very often, but normalises it by the size of the document
+  # to avoid biases to large documents
+  # it also underweights terms that appears to often in many documents
+  # this avoids the appearance of wors such as "say", which often appears in newspapers
+  tfidf = models.TfidfModel(corpus, normalize = True)
+  corpus_tfidf = tfidf[corpus] # apply the trained transformation to the corpus
   
-  #ldamodel = models.ldamodel.LdaModel(corpus, num_topics=ntopics, id2word = dictionary)
-  ldamodel = models.lsimodel.LsiModel(corpus, num_topics=ntopics, id2word = dictionary)
-  topics = ldamodel.print_topics()
+  # now make the model, which can be LSI for an SVD transformation of the 
+  # term-document matrix
+  # or LDA for a probabilistic model
+  if useLDA:
+    myModel = models.ldamodel.LdaModel(corpus_tfidf, num_topics=ntopics, id2word = dictionary)
+  else:
+    myModel = models.lsimodel.LsiModel(corpus_tfidf, num_topics=ntopics, id2word = dictionary)
+
+  # now print the topics that appear often
+  topics = myModel.print_topics()
   for i in range(0, len(topics)):
-    print "Topic #%d: %s" %(i, topics[i])
-    utils.save_fulltopic_graph([ ldamodel.show_topics(ntopics, formatted=False)[i] ], [i], "_only_%d.html" % i)
-  utils.save_fulltopic_graph(ldamodel.show_topics(ntopics, formatted=False), range(0, len(topics)))
-  
-  print "--"
+    print "Topic #%d: %s" %(i, topics[i]) # show it in the screen
+    # make a graph showing this topic connected to its words, with the length
+    # of the edge being the weight of the word in that topic
+    utils.save_fulltopic_graph([ myModel.show_topics(ntopics, formatted=False)[i] ], [i], "_only_%d.html" % i)
+  # same as before, but put all topics and words in the same graph
+  utils.save_fulltopic_graph(myModel.show_topics(ntopics, formatted=False), range(0, len(topics)))
+
+  # Try now projecting the document in the topics set
+  # this tells us how much each topic contributes in a document
   print "Topics per document:"
   topic_per_doc = {}
   for did in range(0, len(texts)):
-    if hasattr(ldamodel, "get_document_topics"):
-      print "Document %s: %s" % (doc_id[did], ldamodel.get_document_topics(dictionary.doc2bow(texts[did])))
+    if useLDA:
+      print "Document %s: %s" % (doc_id[did], myModel.get_document_topics(tfidf[dictionary.doc2bow(texts[did])]))
     else:
-      print "Document %s: %s" % (doc_id[did], ldamodel[dictionary.doc2bow(texts[did])])
+      print "Document %s: %s" % (doc_id[did], myModel[tfidf[dictionary.doc2bow(texts[did])]])
     date = doc_id[did].split('/')[0]
     d = doc_id[did].split('/')[-1]
     if not date in topic_per_doc:
       topic_per_doc[date] = {}
-    if hasattr(ldamodel, "get_document_topics"):
-      topic_per_doc[date][d] = ldamodel.get_document_topics(dictionary.doc2bow(texts[did]))
+    if useLDA:
+      topic_per_doc[date][d] = myModel.get_document_topics(tfidf[dictionary.doc2bow(texts[did])])
     else:
-      topic_per_doc[date][d] = ldamodel[dictionary.doc2bow(texts[did])]
-  for date in topic_per_doc:
-    utils.save_doctopic_graph(topic_per_doc[date], "topic_per_doc_%s.html" % date)
-    utils.save_doctopic_full(topic_per_doc[date], ldamodel.show_topics(ntopics, formatted=False), "topic_per_doc_full_%s.html" % date)
-    utils.save_doctopic_full_nointermediate(topic_per_doc[date], ldamodel.show_topics(ntopics, formatted=False), "topic_per_doc_full_nointermediate_%s.html" % date)
-  
-  utils.save_doc_word_time(topic_per_doc, ldamodel.show_topics(ntopics, formatted=False), ".html")
+      topic_per_doc[date][d] = myModel[tfidf[dictionary.doc2bow(texts[did])]]
+
+  # now do a similarity query
+  if useLDA:
+    corpus_projection = myModel.get_document_topics(corpus_tfidf)
+  else:
+    corpus_projection = myModel[corpus_tfidf]
+  index = similarities.MatrixSimilarity(corpus_projection)
+
+  # we can now use index[input], where input = myModel[tfidf[dictionary.doc2bow(newDocument.lower().split())]]
+  # this compares a new document with what is in the corpus
+  # we can compare the documents in the corpus with each other
+  similar = {}
+  for did in range(0, len(texts)):
+    similar[doc_id[did]] = {}
+    for did2, weight in list(enumerate(index[topic_per_doc[date][d]])):
+      similar[doc_id[did]][doc_id[did2]] = weight
+  utils.save_similarity_graph(similar)
+
+  # now try a similarity query for fixed date
+  similar_date = {}
+  for d in date_set: # for every date available
+    texts_d = []
+    did_d = []
+    # for each document in that date
+    for document in date_set[d]:
+      texts_d.append(texts[document])
+      did_d.append(doc_id[document])
+    # train similarity model
+    new_corpus = [dictionary.doc2bow(text) for text in texts_d]
+    new_corpus_tfidf = tfidf[new_corpus] # apply the trained transformation to the corpus
+    index_d = similarities.MatrixSimilarity(myModel[new_corpus_tfidf])
+    similar_date[d] = {}
+    for idx_id in range(0, len(did_d)):
+      doc_name = did_d[idx_id].split('/')[-1]
+      similar_date[d][doc_name] = {}
+      for did2, weight in list(enumerate(index_d[ myModel[tfidf[dictionary.doc2bow(texts_d[idx_id])]] ])):
+        doc_name2 = did_d[did2].split('/')[-1]
+        similar_date[d][doc_name][doc_name2] = weight
+    utils.save_similarity_graph(similar_date[d], "_%s.html" % d)
+     
+  # the following works, but it is too complicated
+  # not easy to see anything
+  # now make a graph of it
+  # connecting the documents to topics
+  # this is done for each document in a specific day
+  #for date in topic_per_doc:
+  #  # for all documents in this date
+  #  utils.save_doctopic_graph(topic_per_doc[date], "topic_per_doc_%s.html" % date)
+  #  utils.save_doctopic_full(topic_per_doc[date], myModel.show_topics(ntopics, formatted=False), "topic_per_doc_full_%s.html" % date)
+  #  utils.save_doctopic_full_nointermediate(topic_per_doc[date], myModel.show_topics(ntopics, formatted=False), "topic_per_doc_full_nointermediate_%s.html" % date)
+  #
+  #utils.save_doc_word_time(topic_per_doc, ldamodel.show_topics(ntopics, formatted=False), ".html")
 
 if __name__ == "__main__":
   main()
--- utils.py
+++ utils.py
@@ -266,6 +266,7 @@ sources = {
           }
 
 
+# Save a graph with topics connected to words
 def save_fulltopic_graph(topics, tnames, fname = ".html"):
   import networkx as nx
   import math
@@ -288,7 +289,7 @@ def save_fulltopic_graph(topics, tnames, fname = ".html"):
         G.add_node(showWord(word))
         added_word.append(word)
         node_words.append(showWord(word))
-      G.add_edge(showWord(tnames[t]), showWord(word), weight = 10*weight, length=weight)
+      G.add_edge(showWord(tnames[t]), showWord(word), weight=10*weight, length=weight)
   pos=nx.spring_layout(G, scale=1) # positions for all nodes
   for edge in G.edges():
     fig.line(x = [pos[pt][0] for pt in edge],  y = [pos[pt][1] for pt in edge], line_width = 2, line_alpha = 0.5, line_color = "red")
@@ -303,6 +304,38 @@ def save_fulltopic_graph(topics, tnames, fname = ".html"):
              text_font_size = "10px", text_align = "center", text_baseline = "middle")
   save(fig, "topic_all%s" % (fname), title = "")
 
+# make graph showing similarity between documents
+def save_similarity_graph(similar, fname = ".html"):
+  import networkx as nx
+  import math
+  from bokeh.plotting import figure, show, output_file, save
+  from bokeh.resources import CDN
+  output_file(fname, title = "")
+  fig = figure(x_range = (-.1,1.1), y_range = (-.1,1.1), height = 800, width = 800)
+
+  G = nx.Graph()
+  added_doc = []
+  k = similar.keys()
+  for i in range(0, len(k)):
+    for j in range(0, len(k)):
+      if not k[i] in added_doc:
+        G.add_node(k[i])
+        added_doc.append(k[i])
+      if not k[j] in added_doc:
+        G.add_node(k[j])
+        added_doc.append(k[j])
+      G.add_edge(k[i], k[j], weight=10*(1+similar[k[i]][k[j]]), length=similar[k[i]][k[j]])
+  pos=nx.spring_layout(G, scale=1) # positions for all nodes
+  #for edge in G.edges():
+  #  fig.line(x = [pos[pt][0] for pt in edge],  y = [pos[pt][1] for pt in edge], line_width = 2, line_alpha = 0.5, line_color = "red")
+  for node in G.nodes():
+    fc = 'cyan'
+    #fig.circle(x = [pos[node][0]],  y = [pos[node][1]], radius = 0.08, fill_color = fc, alpha = 0.8)
+    fig.text(x = [pos[node][0]],  y = [pos[node][1]], text = [unicode(node)], text_color = 'black', \
+             text_font_size = "10px", text_align = "center", text_baseline = "middle")
+  save(fig, "similarity%s" % (fname), title = "")
+
+# not used from below here
 def save_doctopic_graph(topics, fname = "doctopic_graph.png"):
   import networkx as nx
   #import matplotlib
@@ -327,7 +360,7 @@ def save_doctopic_graph(topics, fname = "doctopic_graph.png"):
         G.add_node(showWord(word))
         added.append(word)
         node_words.append(showWord(word))
-      G.add_edge(showWord(docname), showWord(word), weight = 1*weight)
+      G.add_edge(showWord(docname), showWord(word), weight = 10*weight)
   pos=nx.spring_layout(G, scale=1) # positions for all nodes
   for edge in G.edges():
     fig.line(x = [pos[pt][0] for pt in edge],  y = [pos[pt][1] for pt in edge], line_width = 2, line_alpha = 0.5, line_color = "red")
@@ -375,7 +408,7 @@ def save_doctopic_full_nointermediate(doc_topics, topics, fname = "doctopic_grap
           G.add_node(showWord(word))
           added_word.append(word)
           node_words.append(showWord(word))
-        G.add_edge(showWord(docname), showWord(word), weight = 1*weight_w*weight)
+        G.add_edge(showWord(docname), showWord(word), weight = 10*weight_w*weight)
   pos=nx.spring_layout(G, scale=1) # positions for all nodes
   for edge in G.edges():
     fig.line(x = [pos[pt][0] for pt in edge],  y = [pos[pt][1] for pt in edge], line_width = 2, line_alpha = 0.5, line_color = "red")
@@ -428,8 +461,8 @@ def save_doctopic_full(doc_topics, topics, fname = "doctopic_graph.png"):
             G.add_node(showWord(word))
             added_word.append(word)
             node_words.append(showWord(word))
-          G.add_edge(showWord(topic), showWord(word), weight = 1*weight_w)
-      G.add_edge(showWord(docname), showWord(topic), weight = weight)
+          G.add_edge(showWord(topic), showWord(word), weight = 10*weight_w)
+      G.add_edge(showWord(docname), showWord(topic), weight = 10*weight)
   pos=nx.spring_layout(G, scale=1) # positions for all nodes
   for edge in G.edges():
     fig.line(x = [pos[pt][0] for pt in edge],  y = [pos[pt][1] for pt in edge], line_width = 2, line_alpha = 0.5, line_color = "red")
